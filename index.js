64e916cd453fee15291f88022d2b43bf98b5aa2b
greg1814 <100357429+greg1814@users.noreply.github.com>
2025-08-19 11:44:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const Composer = require('telegraf/composer')
 
-const bot = new Composer()
-
-bot.start(({ reply }) => reply('Welcome message'))
-bot.help(({ reply }) => reply('Help message'))
-bot.settings(({ reply }) => reply('Bot settings'))
-
-bot.command('date', ({ reply }) => reply(`Server time: ${Date()}`))
-
-module.exports = bot
